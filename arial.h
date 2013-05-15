c7de18e4c018b878e05937a457d6dd73919b48b1
Mario <mariolm@gmail.com>
2013-05-15 18:16:35 +0200

    Texto de 6pt en curso
    Se han borrado los anteriores commits

---
+++ arial.h
@@ -0,0 +1,16 @@
+/*
+ * arial.h
+ *
+ *  Created on: 12/05/2013
+ *      Author: Mario
+ */
+
+#ifndef ARIAL_H_
+#define ARIAL_H_
+
+// Font data for Arial 8pt
+extern const unsigned char arial_6ptBitmaps[];
+extern const unsigned int arial_6ptDescriptors[][2];
+//extern const FONT_CHAR_INFO arial_8ptDescriptors[];
+
+#endif /* ARIAL_H_ */
